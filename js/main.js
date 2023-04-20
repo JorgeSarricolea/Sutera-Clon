ae1293c3621feeb3c534f2dc1e83e2a6ed69ac07
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-04-20 00:20:46 -0600

    Step 1 Section

--- js/main.js
+++ js/main.js
@@ -1,40 +1,12 @@
-// Functions to change carousel images
-
+// Functions for carousel images
 const carousel = document.getElementById('carousel');
 const images = carousel.getElementsByTagName('img');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
+const thumbnails = document.getElementsByClassName('thumbnail');
 
 let currentIndex = 0;
 
-// This will be show or not the image
-function showImage(index) {
-    images[currentIndex].style.display = 'none';
-    currentIndex = index;
-    images[currentIndex].style.display = 'block';
-}
-
-// Show prev image
-function prevImage() {
-    const newIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(newIndex);
-}
-
-// Show next image
-function nextImage() {
-    const newIndex = (currentIndex + 1) % images.length;
-    showImage(newIndex);
-}
-
-// Listener to the buttons (next or prev)
-prevButton.addEventListener('click', prevImage);
-nextButton.addEventListener('click', nextImage);
-
-// Thumbnail functions
-
-const thumbnails = document.getElementsByClassName('thumbnail');
-
-// This will be show or not the image, this is necessary so that the thumbnail changes both when clicking with the buttons and on itself
 function showImage(index) {
     images[currentIndex].style.display = 'none';
     currentIndex = index;
@@ -42,30 +14,60 @@ function showImage(index) {
     updateThumbnailSelected();
 }
 
-// This will change the size of the thumbnail (by buttons)
+function prevImage() {
+    const newIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(newIndex);
+}
+
+function nextImage() {
+    const newIndex = (currentIndex + 1) % images.length;
+    showImage(newIndex);
+}
+
 function updateThumbnailSelected() {
     for (let i = 0; i < thumbnails.length; i++) {
         if (i === currentIndex) {
-            thumbnails[i].classList.add('selected');
+        thumbnails[i].classList.add('selected');
         } else {
-            thumbnails[i].classList.remove('selected');
+        thumbnails[i].classList.remove('selected');
         }
     }
 }
 
-// This will change the size of the thumbnail (by user selection)
 function selectImageByThumbnail(event) {
     for (let i = 0; i < thumbnails.length; i++) {
         if (thumbnails[i] === event.target) {
-            showImage(i);
-            updateThumbnailSelected();
-            break;
+        showImage(i);
+        updateThumbnailSelected();
+        break;
         }
     }
 }
 
+// Listener to the buttons (next or prev)
+prevButton.addEventListener('click', prevImage);
+nextButton.addEventListener('click', nextImage);
+
+// Listener to the thumbnails
 for (const thumbnail of thumbnails) {
     thumbnail.addEventListener('click', selectImageByThumbnail);
 }
-updateThumbnailBorder();
 
+// Functions for radio form
+
+var options = document.querySelectorAll('.prices-container .option-price');
+
+// Select default option
+options[2].classList.add('selected');
+
+// Add event click forEach
+options.forEach(function(option) {
+    option.addEventListener('click', function() {
+        // Remove selected class
+        options.forEach(function(o) {
+        o.classList.remove('selected');
+        });
+        // Add selected class
+        this.classList.add('selected');
+    });
+});
